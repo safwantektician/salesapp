a77c09c1658ab68fc8dbab3ec0e7b20d5d757c08
arvindder <arvin6730@gmail.com>
2020-04-09 05:29:46 +0800

    fixing bugs on leads call page

--- src/app/leaddetails/leaddetails.page.ts
+++ src/app/leaddetails/leaddetails.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'
 import { CallNumber } from '@ionic-native/call-number/ngx';
 import { CallLog, CallLogObject } from '@ionic-native/call-log/ngx';
+import { SocketService } from '../api/socket.service'
+
+declare var window: any;
 
 @Component({
   selector: 'app-leaddetails',
@@ -11,30 +14,111 @@ import { CallLog, CallLogObject } from '@ionic-native/call-log/ngx';
 export class LeaddetailsPage implements OnInit {
 
   public data: any
+  public startCall: any;
+  public filters: CallLogObject[];
+  public previousState: string;
   constructor(
     private activateRoute: ActivatedRoute,
     private router: Router,
-    private callNumber: CallNumber
+    private callNumber: CallNumber,
+    private callLog: CallLog,
+    private socket: SocketService
   ) {
     this.activateRoute.params.subscribe(params => {
       this.data = JSON.parse(params.data)
       //this.data = params
       console.log(this.data)
     });
+
+    // Check permission
+    this.callLog.hasReadPermission().then(hasPermission => {
+      if (!hasPermission) {
+        this.callLog.requestReadPermission().then(results => {
+          //this.getContacts("type","1","==");
+          console.log(results);
+        })
+          .catch(e => console.log(" requestReadPermission " + JSON.stringify(e)));
+      }
+    })
+      .catch(e => console.log(" hasReadPermission " + JSON.stringify(e)));
     // console.log(this.activateRoute.snapshot.paramMap.get('data'))
   }
 
-  callLead(number: string)
-  {
+  callLead(number: string) {
     this.callNumber.callNumber(number, false)
-			.then((res) => {
-				console.log('calling');
-			})
-			.catch(err => console.log('Error launching dialer', err));
+      .then((res) => {
+        console.log('calling');
+        this.startCall = new Date().valueOf();
+        if (window.PhoneCallIntercept) {
+          window.PhoneCallIntercept.onCall((state) => {
+            switch (state) {
+              case "RINGING":
+                console.log("Phone is ringing");
+                break;
+              case "OFFHOOK":
+                console.log("Phone is off-hook/Ongoing Call");
+                this.previousState = 'OFFHOOK'
+                this.socket.setBuzyState({
+                  state: 'OFFHOOK',
+                  date: new Date(),
+                  data: this.data
+                })
+                break;
+              case "IDLE":
+                console.log("Phone is idle, call is ended");
+                if (this.previousState == 'OFFHOOK') {
+
+                  this.socket.setBuzyState({
+                    state: 'IDLE',
+                    date: new Date(),
+                    data: this.data
+                  })
+
+                  this.filters = [{
+                    "name": "number",
+                    "value": number,
+                    "operator": "==",
+                  }, {
+                    "name": "date",
+                    "value": this.startCall,
+                    "operator": ">="
+                  }]
+                  setTimeout(() => {
+                    this.callLog.getCallLog(this.filters)
+                      .then((results) => {
+                        //setTimeout(function(){
+                        alert(JSON.stringify(results));
+
+                        this.router.navigate(['/leadcallend', { data: JSON.stringify(this.data), callLog: JSON.stringify(results[0]) }]);
+                        // if (Object.keys(results).length) {
+                        // this.socket.callEnded(JSON.stringify({data : this.data, callLog:results[0]})).subscribe(resp => {
+                        // 	console.log(resp);
+                        // });
+                        //console.log(JSON.stringify(results))
+                        //if(results[0]){
+
+                        //}
+                        //}
+                        //},2000);
+                        // }
+                      })
+                      .catch((e) => {
+                        console.log(e);
+                      });
+                  }, 2000);
+                  // Set back the state to idle
+                  this.previousState = 'IDLE'
+                }
+                break;
+            }
+          });
+        }
+      })
+      .catch(err => console.log('Error launching dialer', err));
     //this.router.navigate(['/leadacceptsuccess', { data: JSON.stringify(this.data) }]);
   }
-  leadAction(){
-      this.router.navigate(['/leadaction', { data: JSON.stringify(this.data) }]);
+  leadAction() {
+    this.router.navigate(['/leadaction', { data: JSON.stringify(this.data) }]);
   }
 
 
